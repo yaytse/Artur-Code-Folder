fbb7800006596ac06f5cfef18a52fd6ba66eef25
yaytse <115877754+yaytse@users.noreply.github.com>
2024-09-18 09:24:32 -0600

    Wednesday work

---
+++ Assigment 6/src/App.java
@@ -0,0 +1,12 @@
+//Assignment 6
+//Artur Mararash
+//09/18/2024
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double balance = 4.75;
+        double cost = 0.75;
+        System.out.println("You can buy " + (int)(balance/cost) + " wings.");
+    }
+}
