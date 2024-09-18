2e6271ae0c007d95a7ee33dd8c5df148a2c6ad01
yaytse <115877754+yaytse@users.noreply.github.com>
2024-09-18 08:52:48 -0600

    Tuesday demo

---
+++ 1-DemosAndExamples/Lesson Variables/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       // System.out.println(1/3);
+       // System.out.println(1.0/3);
+       // System.out.println(1/3.0);
+        //System.out.println(1.0/3.0);
+       // System.out.println((double) 2/3);
+       double myNum = (int) Math.random()*10;
+       System.out.println(myNum);
+    
+    }
+}
--- A4 ASCll ART/src/App.java
+++ A4 ASCll ART/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("    )(        *       |");
+        System.out.println(" *    ()            \\   /");
+        System.out.println("    (    *        --  *   --     ");
+        System.out.println("     )( /\\ *        /   \\");
+        System.out.println("   ()  /__\\           |");
+        System.out.println("*   []/____\\   *            * ");
+        System.out.println("    [/______\\        *");
+        System.out.println("    /________\\                 *");
+        System.out.println("   /__________\\   *       *  ");
+        System.out.println("  /xxxxxxxxxxxx\\");
+        System.out.println(" /{     __     }\\");
+        System.out.println("  { [] I  I [] }   ");
+        System.out.println("  {    I  I    }      ");
+        System.out.println("  ''''''''''''''        ");
+        
     }
 }
