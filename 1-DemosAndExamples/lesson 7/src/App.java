e065b525619b8decdc3d9a3dce7f5041b5f671e9
yaytse <115877754+yaytse@users.noreply.github.com>
2024-09-19 09:49:10 -0600

    Thursday

---
+++ 1-DemosAndExamples/lesson 7/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "This is a test";
+        //String message2 = "Hello class";
+        System.out.println(message1.length());
+        System.out.println(message1.substring(5, 6));
+        System.out.println(message1.indexOf("is"));
+    }
+}
