c60e9a4ea6ede7f9e4401ac0efd2135be897b8c1
yaytse <115877754+yaytse@users.noreply.github.com>
2024-09-20 09:36:27 -0600

    friday

---
+++ assignment7/src/App.java
@@ -0,0 +1,16 @@
+//Assignment #7
+//Artur Mararash
+//09/20/2024
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "I am very happy!";
+        String target1 = "very";
+        int index1 = message1.indexOf(target1);
+        int lenght1 = target1.length();
+        String final1 = message1.substring(0, index1) + message1.substring(index1 + lenght1);
+        System.out.println(final1);
+        //String message2 = "That was great - lol.";
+        //String target2 = "lol";
+    }
+}
