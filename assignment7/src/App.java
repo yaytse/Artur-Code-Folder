046cc44138216fa0f5c1024ef1b92aa5f6802d6e
yaytse <115877754+yaytse@users.noreply.github.com>
2024-09-23 08:55:35 -0600

    Assignment7 update

--- assignment7/src/App.java
+++ assignment7/src/App.java
@@ -8,9 +8,14 @@ public class App {
         String target1 = "very";
         int index1 = message1.indexOf(target1);
         int lenght1 = target1.length();
-        String final1 = message1.substring(0, index1) + message1.substring(index1 + lenght1);
+        String final1 = message1.substring(0, index1-1) + message1.substring(index1 + lenght1);
         System.out.println(final1);
-        //String message2 = "That was great - lol.";
-        //String target2 = "lol";
+
+
+        String message2 = "That was great - lol.";
+        String target2 = "lol";
+        int index2 = message2.indexOf(target2);
+        String final2 = message2.substring(0, index2) + "laugh out loud.";
+        System.out.println(final2);
     }
 }
