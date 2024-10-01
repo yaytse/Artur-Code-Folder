6398e47871b5d414561b822a2ab945a91b2a2898
yaytse <115877754+yaytse@users.noreply.github.com>
2024-10-01 09:37:45 -0600

    tuesday

--- Assignment 8b/src/Secondary_class.java
+++ Assignment 8b/src/Secondary_class.java
@@ -1,11 +1,11 @@
 public class Secondary_class {
     public void Mercedes(){
-        System.out.println("ckew");
+        System.out.println("Interseting fact about Mercedes-Benz: the first automobile was a Mercedes.");
     }
     public void BMW(){
-        System.out.println("ckew");
+        System.out.println("Interesting fact about BMW: they originally built plane engines.");
     }
     public void Audi(){
-        System.out.println("ckew");
+        System.out.println("Interesting fact about Audi: Audi was the first company to crash test their cars.");
     }
 }
