934fe0dae10624a7e7d6255f5e85b8c4a5224a94
yaytse <115877754+yaytse@users.noreply.github.com>
2024-10-03 09:46:53 -0600

    thu

--- Assignment 9/src/BigOrSmall.java
+++ Assignment 9/src/BigOrSmall.java
@@ -1,3 +1,18 @@
+
+//import static java.lang.System.*; 
+//import java.util.Scanner;
+
 public class BigOrSmall {
-    public voi
+    
+    public void check( int a, int b ){
+        if (a>b){
+            System.out.println("Yes");
+        }
+        else if (a < b){
+            System.out.println("No");
+        }
+        else {
+            System.out.println("Equals");
+        }
+    }
 }
--- Assignment 9/src/BigOrSmallRunner.java
+++ Assignment 9/src/BigOrSmallRunner.java
@@ -1,5 +1,19 @@
+//Assignment 8
+//Artur Mararash    
+//10/02/2024
+
+
 public class BigOrSmallRunner {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BigOrSmall numbers = new BigOrSmall();
+        numbers.check( 10, 20);		
+		numbers.check( 20, 10);	
+		numbers.check( 20, 20);
+		numbers.check( 10, 10);	
+		numbers.check( 0, 1);	
+		numbers.check( 1, 0 );	
+		numbers.check( 3, 5);	
+		numbers.check( 5, 3 );	
+		numbers.check( 5342, 323 );
     }
 }
