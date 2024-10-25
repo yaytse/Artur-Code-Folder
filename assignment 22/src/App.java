298fbba42995a71cdc247492fa0bdbdea20daa37
yaytse <115877754+yaytse@users.noreply.github.com>
2024-10-25 09:45:49 -0600

    friday

---
+++ assignment 22/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Methods test = new Methods();
+        test.input();
+        test.checkLeap();
+    }
+}
---
+++ assignment 22/src/Methods.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Methods {
+    Scanner input = new Scanner(System.in);
+    public int userprompt;
+    public void input(){
+    boolean value = false;
+    while (value == false){
+        System.out.print("Enter a year: ");
+        String input1 = input.nextLine();  
+
+        try {
+            userprompt = Integer.valueOf(input1);
+            value = true;
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("Please, input an integer :) ");
+            value = false;
+            }
+        }
+    }
+    public void checkLeap(){
+        if ((userprompt%4 == 0 && userprompt%100!=0) || userprompt%400 == 0){
+            System.out.println(userprompt + " is a leap year.");
+        }
+        else{
+        System.out.println(userprompt + " is NOT a leap year.");
+        }
+    }
+}
